db4ce360f71e94a4c3f295196601301120b94a45
Agustin Dubal <83585791+AgusDubal@users.noreply.github.com>
2025-05-20 14:48:30 -0300

    Feat: create a new class for making coffee

---
+++ IntroductionToSoftwareDevelopment/CoffeeMachinePolymorph/src/CoffeeMaker.java
@@ -0,0 +1,13 @@
+public class CoffeeMaker {
+
+    public void prepareCoffee(Coffee myFavoriteCoffee) {
+
+        // Call the grindBeans() method on the Coffee object
+        myFavoriteCoffee.grindBeans();
+
+        // Call the brewCoffee() method on the Coffee object
+        myFavoriteCoffee.brewCoffee();
+
+    }
+
+}
